ebf7348b8c3546efa30f0cff51ce480f284619f2
aphichat1835-coder <aphichat1835@gmail.com>
2026-08-02 07:57:31 +0000

    Revert "Build Questshop production foundation"
    This reverts commit 48e840e828ad2a6722a0eb5f657310c814ad4dc4.

--- migrations/0005_retention_checkpoints.sql
+++
@@ -1,3 +0,0 @@
-CREATE UNIQUE INDEX wallet_checkpoints_boundary_idx
-  ON wallet_checkpoints(discord_user_id, through_transaction_id);
-
--- scripts/migrate.js
+++
@@ -1,8 +0,0 @@
-import { loadEnvironment } from '../src/config/env.js';
-import { runMigrations } from '../src/db/migrations.js';
-import { closePools } from '../src/db/pools.js';
-
-const env = loadEnvironment();
-try { console.log(await runMigrations({ gitSha: env.GIT_SHA,
-  runtimeRole: decodeURIComponent(new URL(env.DATABASE_POOL_URL).username) })); }
-finally { await closePools(); }
